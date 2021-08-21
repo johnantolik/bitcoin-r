a2e9d8dce3a90b980b9a30362245d748d9d908c1
John Antolik <87459650+johnantolik@users.noreply.github.com>
2021-08-21 14:28:54 -0700

    Update data_exploration.R

--- data_analyses/data_exploration.R
+++ data_analyses/data_exploration.R
@@ -22,8 +22,24 @@ daily <- read_csv("bitcoin_daily-bytetree_api-07.25.21.csv")
 data_daily <- read_csv("data_daily-bitcoin_visuals-08.20.21.csv")
 
 summary(blocks)
-
 summary(daily)
-
 summary(data_daily)
 
+str(blocks)
+str(daily)
+str(data_daily)
+
+glimpse(blocks)
+glimpse(daily)
+glimpse(data_daily)
+
+blocks
+daily
+data_daily
+
+# Examine weekday bias (VS weekend) in blocks db
+
+blocks %>%
+  group_by(date) %>%
+  
+  
